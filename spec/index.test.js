c428b444abe5c093afe12136519a80fd141d539e
Tony Brix <tony@brix.ninja>
2021-01-22 13:04:47 -0600

    use markeds eslint config

--- spec/index.test.js
+++ spec/index.test.js
@@ -1,50 +1,50 @@
-const marked = require("marked");
-const linkify = require("../");
+const marked = require('marked');
+const linkify = require('../');
 
-describe("linkify-it", () => {
-	beforeEach(() => {
-	  marked.setOptions(marked.getDefaults());
-	});
+describe('linkify-it', () => {
+  beforeEach(() => {
+    marked.setOptions(marked.getDefaults());
+  });
 
-	test("no options", () => {
-		marked.use(linkify());
-		expect(marked("example.com")).toEqual(expect.stringMatching("href=\"http://example.com\""));
-	});
+  test('no options', () => {
+    marked.use(linkify());
+    expect(marked('example.com')).toEqual(expect.stringMatching('href="http://example.com"'));
+  });
 
-	test("add schemas", () => {
-		marked.use(linkify({"test:": "http:"}));
-		expect(marked("test://example.com")).toBe("<p><a href=\"test://example.com\">test://example.com</a></p>\n");
-	});
+  test('add schemas', () => {
+    marked.use(linkify({ 'test:': 'http:' }));
+    expect(marked('test://example.com')).toBe('<p><a href="test://example.com">test://example.com</a></p>\n');
+  });
 
-	test("add options", () => {
-		marked.use(linkify({}, {"fuzzyLink": false}));
-		expect(marked("example.com")).toBe("<p>example.com</p>\n");
-	});
+  test('add options', () => {
+    marked.use(linkify({}, { fuzzyLink: false }));
+    expect(marked('example.com')).toBe('<p>example.com</p>\n');
+  });
 
-	test("only domain", () => {
-		marked.use(linkify({}, {tlds: "onion"}));
-		expect(marked("example.com")).toBe("<p>example.com</p>\n");
-		expect(marked("example.onion")).toEqual(expect.stringMatching("href=\"http://example.onion\""));
-	});
+  test('only domain', () => {
+    marked.use(linkify({}, { tlds: 'onion' }));
+    expect(marked('example.com')).toBe('<p>example.com</p>\n');
+    expect(marked('example.onion')).toEqual(expect.stringMatching('href="http://example.onion"'));
+  });
 
-	test("add domain", () => {
-		marked.use(linkify({}, {tlds: "onion", tldsKeepOld: true}));
-		expect(marked("example.com")).toEqual(expect.stringMatching("href=\"http://example.com\""));
-		expect(marked("example.onion")).toEqual(expect.stringMatching("href=\"http://example.onion\""));
-	});
+  test('add domain', () => {
+    marked.use(linkify({}, { tlds: 'onion', tldsKeepOld: true }));
+    expect(marked('example.com')).toEqual(expect.stringMatching('href="http://example.com"'));
+    expect(marked('example.onion')).toEqual(expect.stringMatching('href="http://example.onion"'));
+  });
 
-	test("brackets", () => {
-		marked.use(linkify());
-		expect(marked("<example.com>")).toBe("<p><a href=\"http://example.com\">example.com</a></p>\n");
-	});
+  test('brackets', () => {
+    marked.use(linkify());
+    expect(marked('<example.com>')).toBe('<p><a href="http://example.com">example.com</a></p>\n');
+  });
 
-	test("not first token", () => {
-		marked.use(linkify());
-		expect(marked("at example.com")).toBe("<p>at <a href=\"http://example.com\">example.com</a></p>\n");
-	});
+  test('not first token', () => {
+    marked.use(linkify());
+    expect(marked('at example.com')).toBe('<p>at <a href="http://example.com">example.com</a></p>\n');
+  });
 
-	test("not first token with brackets", () => {
-		marked.use(linkify());
-		expect(marked("at <example.com>")).toBe("<p>at <a href=\"http://example.com\">example.com</a></p>\n");
-	});
+  test('not first token with brackets', () => {
+    marked.use(linkify());
+    expect(marked('at <example.com>')).toBe('<p>at <a href="http://example.com">example.com</a></p>\n');
+  });
 });
--- src/index.js
+++ src/index.js
@@ -1,59 +1,58 @@
-const {inlineText} = require("./helpers.js");
+const { inlineText } = require('./helpers.js');
 
-module.exports = function (schemas = {}, options = {}) {
+module.exports = function(schemas = {}, options = {}) {
+  const linkify = require('linkify-it')(schemas, options);
 
-	const linkify = require("linkify-it")(schemas, options);
+  const tlds = options.tlds;
+  delete options.tlds;
+  const tldsKeepOld = options.tldsKeepOld;
+  delete options.tldsKeepOld;
 
-	const tlds = options.tlds;
-	delete options.tlds;
-	const tldsKeepOld = options.tldsKeepOld;
-	delete options.tldsKeepOld;
+  if (tlds) {
+    linkify.tlds(tlds, tldsKeepOld);
+  }
 
-	if (tlds) {
-		linkify.tlds(tlds, tldsKeepOld);
-	}
+  return {
+    tokenizer: {
+      autolink(src) {
+        const match = linkify.match(src);
 
-	return {
-		tokenizer: {
-			autolink(src) {
-				const match = linkify.match(src);
+        if (!match || !match.length) {
+          return;
+        }
 
-				if (!match || !match.length) {
-					return;
-				}
+        const link = match[0];
 
-				const link = match[0];
+        let raw;
+        if (link.index === 0) {
+          raw = link.raw;
+        } else if (link.index === 1 && src.charAt(0) === '<' && src.charAt(link.lastIndex) === '>') {
+          raw = `<${link.raw}>`;
+        }
 
-				let raw;
-				if (link.index === 0) {
-					raw = link.raw;
-				} else if (link.index === 1 && src.charAt(0) === "<" && src.charAt(link.lastIndex) === ">") {
-					raw = `<${link.raw}>`;
-				}
-
-				if (raw) {
-					return {
-						type: "link",
-						raw,
-						text: link.text,
-						href: link.url,
-						tokens: [
-							{
-								type: "text",
-								raw: link.text,
-								text: link.text,
-							},
-						],
-					};
-				}
-			},
-			inlineText(src, ...args) {
-				const match = linkify.match(src);
-				if (match && match.length > 0) {
-					src = src.substring(0, match[0].index);
-				}
-				return inlineText.call(this, src, ...args);
-			}
-		},
-	};
+        if (raw) {
+          return {
+            type: 'link',
+            raw,
+            text: link.text,
+            href: link.url,
+            tokens: [
+              {
+                type: 'text',
+                raw: link.text,
+                text: link.text
+              }
+            ]
+          };
+        }
+      },
+      inlineText(src, ...args) {
+        const match = linkify.match(src);
+        if (match && match.length > 0) {
+          src = src.substring(0, match[0].index);
+        }
+        return inlineText.call(this, src, ...args);
+      }
+    }
+  };
 };
