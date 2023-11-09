cc27791e72dde1b8ea67853a21266767ead76b95
Tony Brix <tony@brix.ninja>
2023-11-08 20:14:05 -0700

    fix: fix link in link text (#476)

--- spec/index.test.js
+++ spec/index.test.js
@@ -1,55 +1,56 @@
-import { marked } from 'marked';
+import { Marked } from 'marked';
 import markedLinkifyIt from '../src/index.js';
 
 describe('linkify-it', () => {
-  beforeEach(() => {
-    marked.setOptions(marked.getDefaults());
-  });
-
   test('no options', () => {
-    marked.use(markedLinkifyIt());
-    expect(marked('example.com')).toEqual(expect.stringMatching('href="http://example.com"'));
+    const marked = new Marked(markedLinkifyIt());
+    expect(marked.parse('example.com')).toEqual(expect.stringMatching('href="http://example.com"'));
   });
 
   test('add schemas', () => {
-    marked.use(markedLinkifyIt({ 'test:': 'http:' }));
-    expect(marked('test://example.com')).toBe('<p><a href="test://example.com">test://example.com</a></p>\n');
+    const marked = new Marked(markedLinkifyIt({ 'test:': 'http:' }));
+    expect(marked.parse('test://example.com')).toBe('<p><a href="test://example.com">test://example.com</a></p>\n');
   });
 
   test('add options', () => {
-    marked.use(markedLinkifyIt({}, { fuzzyLink: false }));
-    expect(marked('example.com')).toBe('<p>example.com</p>\n');
+    const marked = new Marked(markedLinkifyIt({}, { fuzzyLink: false }));
+    expect(marked.parse('example.com')).toBe('<p>example.com</p>\n');
   });
 
   test('only domain', () => {
-    marked.use(markedLinkifyIt({}, { tlds: 'onion' }));
-    expect(marked('example.com')).toBe('<p>example.com</p>\n');
-    expect(marked('example.onion')).toEqual(expect.stringMatching('href="http://example.onion"'));
+    const marked = new Marked(markedLinkifyIt({}, { tlds: 'onion' }));
+    expect(marked.parse('example.com')).toBe('<p>example.com</p>\n');
+    expect(marked.parse('example.onion')).toEqual(expect.stringMatching('href="http://example.onion"'));
   });
 
   test('add domain', () => {
-    marked.use(markedLinkifyIt({}, { tlds: 'onion', tldsKeepOld: true }));
-    expect(marked('example.com')).toEqual(expect.stringMatching('href="http://example.com"'));
-    expect(marked('example.onion')).toEqual(expect.stringMatching('href="http://example.onion"'));
+    const marked = new Marked(markedLinkifyIt({}, { tlds: 'onion', tldsKeepOld: true }));
+    expect(marked.parse('example.com')).toEqual(expect.stringMatching('href="http://example.com"'));
+    expect(marked.parse('example.onion')).toEqual(expect.stringMatching('href="http://example.onion"'));
   });
 
   test('brackets', () => {
-    marked.use(markedLinkifyIt());
-    expect(marked('<example.com>')).toBe('<p><a href="http://example.com">example.com</a></p>\n');
+    const marked = new Marked(markedLinkifyIt());
+    expect(marked.parse('<example.com>')).toBe('<p><a href="http://example.com">example.com</a></p>\n');
   });
 
   test('not first token', () => {
-    marked.use(markedLinkifyIt());
-    expect(marked('at example.com')).toBe('<p>at <a href="http://example.com">example.com</a></p>\n');
+    const marked = new Marked(markedLinkifyIt());
+    expect(marked.parse('at example.com')).toBe('<p>at <a href="http://example.com">example.com</a></p>\n');
   });
 
   test('not first token with brackets', () => {
-    marked.use(markedLinkifyIt());
-    expect(marked('at <example.com>')).toBe('<p>at <a href="http://example.com">example.com</a></p>\n');
+    const marked = new Marked(markedLinkifyIt());
+    expect(marked.parse('at <example.com>')).toBe('<p>at <a href="http://example.com">example.com</a></p>\n');
   });
 
   test('integration with other tokens', () => {
-    marked.use(markedLinkifyIt());
-    expect(marked('# **example.com**')).toBe('<h1><strong><a href="http://example.com">example.com</a></strong></h1>\n');
+    const marked = new Marked(markedLinkifyIt());
+    expect(marked.parse('# **example.com**')).toBe('<h1><strong><a href="http://example.com">example.com</a></strong></h1>\n');
+  });
+
+  test('not convert link text', () => {
+    const marked = new Marked(markedLinkifyIt());
+    expect(marked.parse('[https://example1.com](https://example2.com)')).toBe('<p><a href="https://example2.com">https://example1.com</a></p>\n');
   });
 });
